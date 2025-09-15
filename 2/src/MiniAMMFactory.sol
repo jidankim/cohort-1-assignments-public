83fb2e8ae08893e4a8ef5453b89871be2173ac9f
jidankim <jidankim@gmail.com>
2025-09-15 12:20:44 +0900

    Implement factory feature

--- 2/src/MiniAMMFactory.sol
+++ 2/src/MiniAMMFactory.sol
@@ -13,14 +13,31 @@ contract MiniAMMFactory is IMiniAMMFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint256 pairNumber);
     
     constructor() {}
-    
+
     // implement
     function allPairsLength() external view returns (uint256) {
-        return 0;
+        return allPairs.length;
     }
     
     // implement
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        return address(0);
+        require(getPair[tokenA][tokenB] == address(0), "Pair exists");
+        require(getPair[tokenB][tokenB] == address(0), "Pair exists");
+        require(tokenA != tokenB, "Identical addresses");
+        require(tokenA != address(0), "Zero address");
+        require(tokenB != address(0), "Zero address");
+
+        MiniAMM pairContract = new MiniAMM(tokenA, tokenB);
+        address tokenX = pairContract.tokenX();
+        address tokenY = pairContract.tokenY();
+
+        address pair = address(pairContract);
+        allPairs.push(pair);
+        getPair[tokenX][tokenY] = pair;
+        getPair[tokenY][tokenX] = pair;
+
+        emit PairCreated(tokenX, tokenY, pair, allPairs.length);
+
+        return pair;
     }
 }
